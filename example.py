98e6b01e71947c93f41410e6d767e84a9f0c9664
kouhei <murakou320@gmail.com>
2025-07-23 10:32:04 +0900

    example.pyとREADME.mdを追加しました

---
+++ example.py
@@ -0,0 +1,25 @@
+
+
+def greet():
+    return "こんにちは"
+
+print(greet())
+
+
+def print_name(name):
+    print(f"私の名前は{name}です")
+
+name = "村山"
+
+print_name(name)
+
+def get_greet():
+    return "おはようございます"
+print(get_greet())
+
+
+def add(a, b):
+    sum = a + b
+    return sum
+
+print(add(2, 3))
